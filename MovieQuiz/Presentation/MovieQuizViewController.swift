e357ba596bdf4fe0439017208fdba16ed83efb82
fearmuch <chervonnyi8@gmail.com>
2025-03-26 23:42:48 +0700

    Изменем Content Mode на Aspect Fill, изменен порядок свойств и методов, mock-данные и структуры были вынесены в отдельные файлы, добавлено свойство isEnabled для устранения спам-клика

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,34 +1,87 @@
 import UIKit
 
 final class MovieQuizViewController: UIViewController {
-    // MARK: - Lifecycle
+    
+    // MARK: - Properties
+    
+    @IBOutlet private weak var textLabel: UILabel!
+    @IBOutlet private weak var counterLabel: UILabel!
+    @IBOutlet private weak var imageView: UIImageView!
+    @IBOutlet weak var noButtonClickedOutlet: UIButton!
+    @IBOutlet weak var yesButtonClickedOutlet: UIButton!
+    
+    private var currentQuestionIndex = 0
+    private var correctAnswers = 0
+    private let questions = QuizQuestionMock.questions
+    
+    // MARK: - Lyfecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         show(quiz: convert(model: questions[0]))
     }
     
+    //MARK: - Actions
+    
     @IBAction private func yesButtonClicked(_ sender: Any) {
         let currentQuestion = questions[currentQuestionIndex]
         let givenAnswer = true
         
+        yesButtonClickedOutlet.isEnabled = false
+        noButtonClickedOutlet.isEnabled = false
+        
         showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            self.yesButtonClickedOutlet.isEnabled = true
+            self.noButtonClickedOutlet.isEnabled = true
+        }
     }
     
     @IBAction private func noButtonClicked(_ sender: Any) {
         let currentQuestion = questions[currentQuestionIndex]
         let givenAnswer = false
         
+        yesButtonClickedOutlet.isEnabled = false
+        noButtonClickedOutlet.isEnabled = false
+        
         showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            self.yesButtonClickedOutlet.isEnabled = true
+            self.noButtonClickedOutlet.isEnabled = true
+        }
     }
     
-    @IBOutlet private weak var textLabel: UILabel!
-    @IBOutlet private weak var counterLabel: UILabel!
-    @IBOutlet private weak var imageView: UIImageView!
+    //MARK: - Setup Methods
     
-    private var currentQuestionIndex = 0
-    private var correctAnswers = 0
+    private func show(quiz step: QuizStep) {
+        imageView.image = step.image
+        textLabel.text = step.question
+        counterLabel.text = step.questionNumber
+    }
     
-    private func show(quiz result: QuizResultsViewModel) {
+    private func showAnswerResult(isCorrect: Bool) {
+        if isCorrect {
+            correctAnswers += 1
+        }
+        
+        imageView.layer.masksToBounds = true // даём разрешение на рисование рамки
+        imageView.layer.borderWidth = 8 // толщина рамки
+        let color = isCorrect ? UIColor.ypGreen : UIColor.ypRed
+        imageView.layer.borderColor = color.cgColor
+        imageView.layer.cornerRadius = 20 // радиус скругления углов рамки
+        
+        // запускаем задачу через 1 секунду c помощью диспетчера задач
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            // код, который мы хотим вызвать через 1 секунду
+            self.showNextQuestionOrResults()
+        }
+    }
+    
+    //MARK: - Private Helpers
+    
+    private func show(quiz result: QuizResults) {
         let alert = UIAlertController(
             title: result.title,
             message: result.text,
@@ -48,10 +101,10 @@ final class MovieQuizViewController: UIViewController {
         
         self.present(alert, animated: true, completion: nil)
     }
-
     
-    private func convert(model: QuizQuestion) -> QuizStepViewModel {
-        let questionStep = QuizStepViewModel(
+    
+    private func convert(model: QuizQuestion) -> QuizStep {
+        let questionStep = QuizStep(
             image: UIImage(named: model.image) ?? UIImage(),
             question: model.text,
             questionNumber: "\(currentQuestionIndex + 1)/\(questions.count)"
@@ -59,29 +112,6 @@ final class MovieQuizViewController: UIViewController {
         return questionStep
     }
     
-    private func show(quiz step: QuizStepViewModel) {
-        imageView.image = step.image
-        textLabel.text = step.question
-        counterLabel.text = step.questionNumber
-    }
-    
-    private func showAnswerResult(isCorrect: Bool) {
-        if isCorrect {
-            correctAnswers += 1
-        }
-        
-        imageView.layer.masksToBounds = true // даём разрешение на рисование рамки
-        imageView.layer.borderWidth = 8 // толщина рамки
-        let color = isCorrect ? UIColor.ypGreen : UIColor.ypRed
-        imageView.layer.borderColor = color.cgColor
-        imageView.layer.cornerRadius = 20 // радиус скругления углов рамки
-        
-        // запускаем задачу через 1 секунду c помощью диспетчера задач
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-           // код, который мы хотим вызвать через 1 секунду
-           self.showNextQuestionOrResults()
-        }
-    }
     
     private func showNextQuestionOrResults() {
         if currentQuestionIndex == questions.count - 1 {
@@ -89,7 +119,7 @@ final class MovieQuizViewController: UIViewController {
             let title = "Этот раунд окончен!"
             let text = "Ваш результат: \(correctAnswers)/10"
             let buttonText = "Сыграть еще раз"
-            let viewModel = QuizResultsViewModel(title: title, text: text, buttonText: buttonText)
+            let viewModel = QuizResults(title: title, text: text, buttonText: buttonText)
             imageView.layer.borderColor = UIColor.clear.cgColor
             show(quiz: viewModel)
         } else {
@@ -99,49 +129,8 @@ final class MovieQuizViewController: UIViewController {
             show(quiz: convert(model: questions[currentQuestionIndex]))
         }
     }
-    
-    struct QuizResultsViewModel {
-        let title: String
-        let text: String
-        let buttonText: String
-    }
-    
-    
-    //вью модель для состояния "Вопрос показан"
-    struct QuizStepViewModel {
-      let image: UIImage
-      let question: String
-      let questionNumber: String
-    }
-    struct QuizQuestion {
-        let image: String
-        let text: String
-        let correctAnswer: Bool
-    }
-
-    private let questions: [QuizQuestion] = [
-        QuizQuestion(
-            image: "The Godfather", text: "Рейтинг этого фильма больше, чем 6?", correctAnswer: true),
-        QuizQuestion(
-            image: "The Dark Knight", text: "Рейтинг этого фильма больше, чем 6?", correctAnswer: true),
-        QuizQuestion(
-            image: "Kill Bill", text: "Рейтинг этого фильма больше, чем 6?", correctAnswer: true),
-        QuizQuestion(
-            image: "The Avengers", text: "Рейтинг этого фильма больше, чем 6?", correctAnswer: true),
-        QuizQuestion(
-            image: "Deadpool", text: "Рейтинг этого фильма больше, чем 6?", correctAnswer: true),
-        QuizQuestion(
-            image: "The Green Knight", text: "Рейтинг этого фильма больше, чем 6?", correctAnswer: true),
-        QuizQuestion(
-            image: "Old", text: "Рейтинг этого фильма больше, чем 6?", correctAnswer: false),
-        QuizQuestion(
-            image: "The Ice Age Adventures of Buck Wild", text: "Рейтинг этого фильма больше, чем 6?", correctAnswer: false),
-        QuizQuestion(
-            image: "Tesla", text: "Рейтинг этого фильма больше, чем 6?", correctAnswer: false),
-        QuizQuestion(
-            image: "Vivarium", text: "Рейтинг этого фильма больше, чем 6?", correctAnswer: false)
-    ]
 }
+
 
 
 
