34eb96c4a01103f13dff9c9e9247da0974e5c171
fearmuch <chervonnyi8@gmail.com>
2025-09-29 23:43:34 +0700

    все хорошо

---
+++ MovieQuizPresenterTests/MovieQuizPresenterTests.swift
@@ -0,0 +1,16 @@
+//
+//  MovieQuizPresenterTests.swift
+//  MovieQuizPresenterTests
+//
+//  Created by Аркадий Червонный on 29.09.2025.
+//
+
+import Testing
+
+struct MovieQuizPresenterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
